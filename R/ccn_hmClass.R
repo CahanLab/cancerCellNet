a611c7fcf5ba51c24c55850626a0c902c7658087
Dan <danpeng28@gmail.com>
2018-10-29 20:56:03 -0400

    fixed the bug with cn_HmClass

--- R/ccn_hmClass.R
+++ R/ccn_hmClass.R
@@ -4,14 +4,14 @@
 #' This function generates a heatmap of the classification result for visualization
 #'
 #' @param classMat classification matrix generated from \code{\link{rf_classPredict}}
-#' @param grps
+#' @param grps a vector that maps samples to a group 
 #' @param isBig TRUE if this is a big heatmap
-#' @param cRow Boolean values determining if rows should be clustered
-#' @param cCol Boolean values determining if columns should be clustered
-#' @param fontsize_row
-#' @param scale
+#' @param cRow TRUE if rows should be clustered
+#' @param cCol TRUE if columns should be clustered
+#' @param fontsize_row the font size 
+#' @param scale FALSE if the highest value is 1 and the lowest is 0 
 #'
-#' @return nothing
+#' @return classification heatmap 
 #'
 #' @examples
 #' ccn_HmClass(cnRes, isBig=TRUE)
@@ -25,12 +25,12 @@ ccn_hmClass<-function(classMat, grps=NULL, isBig=FALSE, cRow=FALSE, cCol=FALSE, 
     bcol<-NA;
   }
 
+  # if no groups specified, use simple heatmap 
   if(is.null(grps)){
-    cn_HmClass(classMat,isBig=isBig)
+    cn_HmClass(classMat,isBig=isBig, cRow=cRow, cCol=cCol, fontsize_row=fontsize_row)
   }
 
   else{
-
     grps<-grps[order(grps)]
     cells<-names(grps)
     groupNames<-sort(unique(grps))
@@ -51,8 +51,13 @@ ccn_hmClass<-function(classMat, grps=NULL, isBig=FALSE, cRow=FALSE, cCol=FALSE, 
       mymax<-1
     }
 
-    pheatmap(classMat, col=cools, breaks=seq(from=mymin, to=mymax, length.out=100), cluster_rows = cRow, cluster_cols = cCol,
-             show_colnames = FALSE, annotation_names_row = FALSE,
+    pheatmap(classMat, 
+             col=cools,  
+             breaks=seq(from=mymin, to=mymax, length.out=100), 
+             cluster_rows = cRow, 
+             cluster_cols = cCol,
+             show_colnames = FALSE, 
+             annotation_names_row = FALSE,
              ##        annotation_col = annTab,
              annotation_col = xx,
              annotation_names_col = FALSE, annotation_colors = anno_colors, fontsize_row=fontsize_row)
@@ -60,33 +65,42 @@ ccn_hmClass<-function(classMat, grps=NULL, isBig=FALSE, cRow=FALSE, cCol=FALSE, 
 
 }
 
-#' simple heatmap of the classification result
+#' Simple heatmap of the classification result
 #'
-#' Heatmap of the classification result.
-#' @param cnRes returned from cn_sapply
-#' @param isBig is this a big heatmap? TRUE or FALSE
+#' Heatmap of the classification result that does not have an explicit group. 
+#' 
+#' @param classMat classification matrix generated from \code{\link{rf_classPredict}}
+#' @param isBig TRUE if this is a big heatmap
+#' @param cRow TRUE if rows should be clustered
+#' @param cCol TRUE if columns should be clustered
+#' @param fontsize_row the font size 
+#' @param scale FALSE if the highest value is 1 and the lowest is 0 
 #'
-#' @return nothing
-#'
-#' @examples
-#' cn_HmClass(cnRes, isBig=TRUE)
+#' @return classification heatmap 
 #'
 #' @export
-cn_HmClass<-function
-(classRes,
- isBig=FALSE
-){
-
+cn_HmClass<-function(classRes, isBig=FALSE, cRow=FALSE, cCol=FALSE, fontsize_row=4, scale=FALSE){
   cools<-colorRampPalette(c("black", "limegreen", "yellow"))( 100 )
   bcol<-'white';
+
   if(isBig){
     bcol<-NA;
   }
+
+  if(scale){
+  mymin<-min(classMat)
+  mymax<-max(classMat)
+  }else{
+    mymin<-0
+    mymax<-1
+  }
+
   pheatmap(classRes,
            col=cools,
-           breaks=seq(from=0, to=1, length.out=100),
+           breaks=seq(from=mymin, to=mymax, length.out=100),
            border_color=bcol,
-           cluster_rows = FALSE,
-           cluster_cols = FALSE)
+           cluster_rows = cRow,
+           cluster_cols = cCol, 
+           fontsize_row = fontsize_row)
   # classification heatmap
 }
