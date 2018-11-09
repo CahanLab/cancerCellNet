77c1ee725a83cef632d9eb33c536ac49c0e699bc
dpeng28 <42895933+dpeng28@users.noreply.github.com>
2018-11-08 20:51:26 -0500

    cleaned rf_classPredict

--- R/rf_classPredict.R
+++ R/rf_classPredict.R
@@ -1,13 +1,14 @@
 
 #' @title
-#' Classify the samples
+#' Classify Query Samples
 #'
 #' @description
-#' This functions will classify the query samples using the random forest object created from \code{\link{makeClassifier}}
+#' Classify the transformed query data using the random forest classifier.
 #'
 #' @param rfObj random forest object created from \code{\link{makeClassifier}}
 #' @param expQuery transformed query data created from \code{\link{query_transform}}
-#' @param numRand number of random profiles wish to create
+#' @param numRand number of random profiles to add into the classification
+#'
 #' @return a classification matrix of the query data
 #' @export
 rf_classPredict<-function(rfObj, expQuery, numRand=50) {
