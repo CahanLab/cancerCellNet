82a22827843911bd241f8fb8daa75c9f054362ff
dpeng28 <42895933+dpeng28@users.noreply.github.com>
2018-11-08 20:46:03 -0500

    documented makeClassifier

--- R/makeClassifier.R
+++ R/makeClassifier.R
@@ -1,20 +1,26 @@
-
-#' make a classifier, with a randomized class too
-#' make a classifier, with a randomized class too
-#' @param expTrain training data
-#' @param genes vector of genes to use as predictors
-#' @param groups named vector of cells to groups or classes
-#' @param nRand =50 num of randomized profiles to make
-#' @param ntrees =2000 number of trees to build
+#' @title
+#' Make Classifier
+#'
+#' @description
+#' Create a random forest classifier with the transformed training data from \code{\link{query_transform}}.
+#'
+#' @param expTrain transformed training data from \code{\link{query_transform}}
+#' @param genes vector of gene pairs from \code{\link{ptGetTop}} used as predictors
+#' @param groups named vector of cells to cancer categories
+#' @param nRand number of randomized profiles to make
+#' @param ntrees number of trees to build
 #' @importFrom randomForest randomForest
+#'
 #' @return Random Forest Classifier object
 #' @export
 makeClassifier<-function(expTrain, genes, groups, nRand=50, ntrees=2000){
   randDat<-randomize(expTrain, num=nRand) # randomizes the
   expTrain<-cbind(expTrain, randDat)
+
   allgenes<-rownames(expTrain)
   missingGenes<-setdiff(unique(genes), allgenes)
   cat("Number of missing genes ", length(missingGenes),"\n")
   ggenes<-intersect(unique(genes), allgenes)
-  randomForest(t(expTrain[ggenes,]), as.factor(c(groups, rep("rand", ncol(randDat)))), ntree=ntrees)
+
+  randomForest::randomForest(t(expTrain[ggenes,]), as.factor(c(groups, rep("rand", ncol(randDat)))), ntree=ntrees)
 }
