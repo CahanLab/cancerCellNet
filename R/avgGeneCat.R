663a0e83612dc615b72a41c5932862e4febb1eb6
Dan  Peng <danpeng@Patricks-iMac.local>
2019-01-11 15:38:36 -0500

    modified avgGeneCat

--- R/avgGeneCat.R
+++ R/avgGeneCat.R
@@ -8,28 +8,45 @@
 #' @param dLevel the name of the column that contains categories
 #' @param sampID the name of the column that contains sample ID
 #'
-#' @return a matrix containing the average of
+#' @return a list containing average matrix and standard matrix
 #' @export
 avgGeneCat<-function(expDat, sampTab, dLevel, sampID){
 
+   returnList = list()
+
    returnMatrix = matrix(nrow=nrow(expDat), ncol=0)
    rownames(returnMatrix) = rownames(expDat)
+
+   stdMatrix = matrix(nrow=nrow(expDat), ncol=0)
+   rownames(stdMatrix) = rownames(expDat)
+
 
    for (cat in unique(sampTab[, dLevel])) {
 
       tempExpDat = expDat[, sampTab[sampTab[, dLevel] == cat, sampID]]
 
+      # calculates the mean
       tempMatrix = matrix(apply(tempExpDat, 1, mean), ncol = 1, nrow = length(apply(tempExpDat, 1, mean)))
-
       rownames(tempMatrix) = names(apply(tempExpDat, 1, mean))
       colnames(tempMatrix) = paste0(cat, "_Avg")
+
+      #calculates the standard deviation
+      tempStd = matrix(apply(tempExpDat, 1, sd), ncol = 1, nrow = length(apply(tempExpDat, 1, sd)))
+      rownames(tempStd) = names(apply(tempExpDat, 1, sd))
+      colnames(tempStd) = paste0(cat, "_Std")
 
       if(all(rownames(returnMatrix) == rownames(tempMatrix))) {
          returnMatrix = cbind(returnMatrix, tempMatrix)
       }
+
+      if(all(rownames(stdMatrix) == rownames(tempStd))) {
+        stdMatrix = cbind(stdMatrix, tempStd)
+      }
    }
 
+   returnList[["Avg_Matrix"]] = returnMatrix
+   returnList[["Std_Matrix"]] = stdMatrix
    #return
-   returnMatrix
+   returnList
 }
 
