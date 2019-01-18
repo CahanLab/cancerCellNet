4bb5fbb688b76d5b3234ef839b0f052c1201b84f
Dan  Peng <danpeng@Patricks-iMac.local>
2019-01-18 14:03:56 -0500

    first round of testing DONE

--- R/subClass_predict.R
+++ R/subClass_predict.R
@@ -1,17 +1,17 @@
-#' @title 
-#' Predict sub class query using broad class and subclass classifier 
+#' @title
+#' Predict sub class query using broad class and subclass classifier
 #' @description
-#' The function predicts the query data using the broad class and subclass classifier 
-#' @param cnProc_broad the cnProc of the broad classifier 
-#' @param cnProc_subclass the cnProc of the subclass classifier 
-#' @param expDat the expression data of query data 
-#' @param nrand the number of random profiles generate for evaluation process 
-#' @return a classification score matrix 
+#' The function predicts the query data using the broad class and subclass classifier
+#' @param cnProc_broad the cnProc of the broad classifier
+#' @param cnProc_subclass the cnProc of the subclass classifier
+#' @param expDat the expression data of query data
+#' @param nrand the number of random profiles generate for evaluation process
+#' @return a classification score matrix
 #'
 #' @export
 
 subClass_predict<-function(cnProc_broad, cnProc_sub, expDat, nrand = 2) {
-   ccnList = cnProc_sub # only concerns with subclass classifier 
+   ccnList = cnProc_sub # only concerns with subclass classifier
 
    rf_tsp<-ccnList[['classifier']]
    cgenes<-ccnList[['cgenes']]
@@ -23,6 +23,6 @@ subClass_predict<-function(cnProc_broad, cnProc_sub, expDat, nrand = 2) {
    expValTrans = subClassQuery_transform(expDat = expDat, cgenes = cgenes, xpairs = xpairs, classMatrix = classMatrix)
 
    returnMatrix = rf_classPredict(rf_tsp, expValTrans, numRand=nrand)
-   #return 
+   #return
    returnMatrix
 }
--- R/subClass_train.R
+++ R/subClass_train.R
@@ -1,27 +1,27 @@
 #' @title
-#' Sub-Class Training 
+#' Sub-Class Training
 #' @description
-#' Tranining sub-class classifier 
+#' Tranining sub-class classifier
 #' @param cnProc_broad the broad cnProc from \code{\link{broadClass_train}}
-#' @param stTrain a dataframe that matches the samples with broad category and sub-class 
-#' @param expTrain the expression matrix 
-#' @param colName_broadCat the name of the column in sample table that contains broad categories 
-#' @param colName_subClass the name of the column in sample table that contains sub class 
+#' @param stTrain a dataframe that matches the samples with broad category and sub-class
+#' @param expTrain the expression matrix
+#' @param colName_broadCat the name of the column in sample table that contains broad categories
+#' @param colName_subClass the name of the column in sample table that contains sub class
 #' @param name_broadCat the name of the broad class in which the subclasses are
-#' @param colName_samp the name of the column that contains sample names 
-#' @param nTopGenes the number of classification genes per category 
-#' @param nTopGenePairs the number of top gene pairs per category 
-#' @param nRand number of random profiles generate for training 
-#' @param nTrees number of trees for random forest classifier 
-#' @param weightDown_total numeric post transformation sum of read counts for weighted_down function 
-#' @param weightedDown_dThresh the threshold at which anything lower than that is 0 for weighted_down function 
+#' @param colName_samp the name of the column that contains sample names
+#' @param nTopGenes the number of classification genes per category
+#' @param nTopGenePairs the number of top gene pairs per category
+#' @param nRand number of random profiles generate for training
+#' @param nTrees number of trees for random forest classifier
+#' @param weightDown_total numeric post transformation sum of read counts for weighted_down function
+#' @param weightedDown_dThresh the threshold at which anything lower than that is 0 for weighted_down function
 #' @param transprop_xFact scaling factor for transprop
-#' 
+#'
 #' @return a list containing normalized expression data, classification gene list, cnPRoc
-#' @export 
+#' @export
 subClass_train<-function(cnProc_broad, stTrain, expTrain, colName_broadCat, colName_subClass, name_broadCat, colName_samp, nTopGenes = 20, nTopGenePairs = 50, nRand = 40, nTrees = 1000, weightedDown_total = 5e5, weightedDown_dThresh = 0.25, transprop_xFact = 1e5) {
    if (class(stTrain) != "data.frame") {
-      stTrain = as.data.frame(stTrain) 
+      stTrain = as.data.frame(stTrain)
    }
    rownames(stTrain) = stTrain[, colName_samp]
    cat("Sample table has been prepared\n")
@@ -33,14 +33,14 @@ subClass_train<-function(cnProc_broad, stTrain, expTrain, colName_broadCat, colN
    expTnorm_sub = expTnorm[, rownames(stTrain_sub)]
    cat("The sub-class expression data has been partitioned\n")
 
-   system.time(cgenes<-findClassyGenes(expDat = expTnorm_sub, sampTab = stTrain_sub, dLevel = colName_cat, topX = nTopGenes))
+   system.time(cgenes<-findClassyGenes(expDat = expTnorm_sub, sampTab = stTrain_sub, dLevel = colName_subClass, topX = nTopGenes))
    cat("Finished finding classification genes\n")
    cgenesA<-cgenes[['cgenes']]
    grps<-cgenes[['grps']]
 
    cat("There are ", length(cgenesA), " classification genes\n")
 
-   system.time(xpairs<-ptGetTop(expTnorm_sub[cgenesA,], grps, topX=50, sliceSize=2000))
+   system.time(xpairs<-ptGetTop(expTnorm_sub[cgenesA,], grps, topX=nTopGenePairs, sliceSize=2000))
    cat("Finished finding top gene pairs\n")
 
    # some of these might include selection cassettes; remove them
@@ -53,7 +53,8 @@ subClass_train<-function(cnProc_broad, stTrain, expTrain, colName_broadCat, colN
    classMatrix = broadClass_predict(cnProc = cnProc_broad, expDat = expTrain)
    classMatrix = classMatrix[, -grep("rand", colnames(classMatrix))]
 
-   expValTrans = subClassQuery_transform(expDat = expTrain, cgenes = cgenes, xpairs = xpairs, classMatrix = classMatrix)
+   cat("Start SubClass Query Transform\n")
+   expValTrans = subClassQuery_transform(expDat = expTrain, cgenes = cgenesA, xpairs = xpairs, classMatrix = classMatrix)
    cat("Features have been selected\n")
 
    newFeatures = c(xpairs, rownames(classMatrix))
@@ -69,6 +70,6 @@ subClass_train<-function(cnProc_broad, stTrain, expTrain, colName_broadCat, colN
    returnList = list("expTnorm" = expTnorm, "cgenes_list" = cgenes, "cnProc_subClass" = cnProc_subClass)
 
    cat("All Done \n")
-   # return 
+   # return
    returnList
 }
