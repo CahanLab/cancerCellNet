9cda0fb471c01603324102a9c0e9027ca0084b75
Dan <dpeng5@jhu.edu>
2019-05-27 20:35:49 -0400

    Trying to revert back to where  I got the project

--- R/CCN.R
+++ R/CCN.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # cancerCellNet
 # (C) Patrick Cahan 2016-2018
 
@@ -12,3 +13,426 @@
 
 
 
+=======
+# cancerCellNet
+# (C) Patrick Cahan 2016-2018
+
+
+
+#' makes vector of gene pairs, iterates over this and computes pairDat, sc_testPattern, then, at the end, findBestPairs 
+#'
+#' @param expDat expDat
+#' @param cell_labels named vector, value is grp, name is cell name
+#' @param topX 50
+#'
+#' @return vector of gene-pair names
+#'
+#' @export
+ptGetTop<-function
+(expDat,
+ cell_labels,
+ topX=50,
+ sliceSize = 5e3){
+
+	ans<-vector()
+
+	# make a data frame of pairs of genes that will be sliced later
+	cat("Making pairTable\n")
+	ngenes<-nrow(expDat)
+	genes<-rownames(expDat)
+	genes1<-vector()
+	genes2<-vector()
+	for(i in 1:ngenes){
+		for(j in 1:ngenes){
+			if(j>i){
+				genes1<-append(genes1, genes[i])
+				genes2<-append(genes2, genes[j])				
+			}
+		}
+	}
+
+	pairTab = data.frame(genes1=genes1, genes2=genes2)
+	pairNames<-paste(pairTab[,1], "_",pairTab[,2], sep='')
+	pairTab <- cbind(pairTab, pairName=pairNames)
+
+	###
+	# setup tmp ans list of sc_testPattern
+	cat("setup ans and make pattern\n")
+	grps<-unique(cell_labels)
+	myPatternG<-sc_sampR_to_pattern(as.character(cell_labels))
+	statList<-list()
+	for(grp in grps){
+		statList[[grp]]<-data.frame()
+	}
+
+	# make the pairedDat, and run sc_testPattern
+	cat("make pairDat on slice and test\n")
+	nPairs = nrow(pairTab)
+	cat("nPairs = ",nPairs,"\n")
+	str = 1
+	stp = min(c(sliceSize, nPairs))
+	while(str <= nPairs){
+		if(stp>nPairs){
+			stp <- nPairs
+		}
+		cat(str,"-", stp,"\n")
+		tmpTab<-pairTab[str:stp,]
+		tmpPdat<-ptSmall(expDat, tmpTab)
+
+		for(gi in seq(length(myPatternG))){
+	    	
+	    	grp<-grps[[gi]]
+    		statList[[grp]]<-rbind( statList[[grp]], sc_testPattern(myPatternG[[gi]], expDat=tmpPdat) )
+    	}
+
+
+    	str = stp+1
+   	 	stp = str + sliceSize - 1		
+	}
+	cat("compile results\n")
+	for(grp in grps){
+    	tmpAns<-findBestPairs(statList[[grp]], topX)
+    	ans<-append(ans, tmpAns)
+    }
+    unique(ans)
+}
+
+ptSmall<-function
+(expDat,
+ pTab){
+	npairs = nrow(pTab)
+	ans<-matrix(0, nrow=npairs, ncol=ncol(expDat))
+	genes1<-as.vector(pTab$genes1)
+	genes2<-as.vector(pTab$genes2)
+
+    for(i in seq(nrow(pTab))){
+    	#cat(genes1[i], ": ", genes2[i],"\n")
+    	ans[i,]<-as.numeric(expDat[genes1[i],]>expDat[genes2[i],]) 
+    }
+	colnames(ans)<-colnames(expDat)
+	rownames(ans)<-as.vector(pTab$pairName)
+	ans
+}
+
+
+
+#' finds the best pairs to use
+#'
+#' @param xdiff xdiff
+#' @param n number of pairs
+#' @param maxPer indicates the number of pairs that a gene is allowed to be in
+#'
+#' @return vector of good pairs
+#'
+#' @export
+findBestPairs<-function # find best and diverse set of pairs
+(xdiff,
+ n=50,
+ maxPer=3){
+ 
+ 	
+ 	xdiff<-xdiff[order(xdiff$cval, decreasing=TRUE),]
+ 	genes<-unique(unlist(strsplit(rownames(xdiff), "_")))
+ 	countList <- rep(0, length(genes))
+ 	names(countList) <- genes	
+
+ 	i<-0
+ 	ans<-vector()
+ 	xdiff_index<-1
+ 	pair_names<-rownames(xdiff)
+ 	while(i < n ){
+ 		tmpAns<-pair_names[xdiff_index]
+ 		tgp <- unlist(strsplit(tmpAns, "_"))
+ 		if( (countList[ tgp[1] ] < maxPer) & (countList[ tgp[2] ] < maxPer )){
+ 			ans<-append(ans, tmpAns)
+ 			countList[ tgp[1] ] <- countList[ tgp[1] ]+ 1
+ 			countList[ tgp[2] ] <- countList[ tgp[2] ]+ 1
+ 			i<-i+1
+ 		}
+ 		xdiff_index <- xdiff_index + 1
+ 	}
+ 	ans
+ }
+
+
+#' find best pairs
+#'
+#' find best pairs
+#'
+#' @param expDat expDat
+#' @param cellLabels named vector of cell groups
+#'
+#' @return vector of pairs
+#' 
+#' @export
+gnrBP<-function(
+  expDat,
+  cellLabels,
+  topX=50){
+
+	ans<-vector()
+    myPatternG<-sc_sampR_to_pattern(as.character(cellLabels))
+    for(i in seq(length(myPatternG))){
+    	cat(i,"\n")
+    	xres<-sc_testPattern(myPatternG[[i]], expDat=expDat)
+    	tmpAns<-findBestPairs(xres, topX)
+    	ans<-append(ans, tmpAns)
+    }
+    unique(ans)
+}
+
+#' getClassGenes
+#'
+#' extract genes for training classifier
+#' @param diffRes a df with cval, holm, rownames=genes
+#' @param topX number of genes to select
+#' @param bottom boolean if ture use the top x genes with - cvals
+#'
+#' @return vector of genes
+#'
+#' @export
+getClassGenes<-function(
+  diffRes,
+  topX=25,
+  bottom=TRUE)
+  {
+    #exclude NAs
+    xi<-which(!is.na(diffRes$cval))
+    diffRes<-diffRes[xi,]   
+    diffRes<-diffRes[order(diffRes$cval, decreasing=TRUE),]
+    ans<-rownames(diffRes[1:topX,])
+    if(bottom){
+      ans<-append(ans, rownames( diffRes[nrow(diffRes) - ((topX-1):0),]))
+    }
+    ans
+  }
+
+
+#' find candidate classifier-worthy genes
+#'
+#' find candidate classifier-worthy genes
+#'
+#' @param expDat expDat
+#' @param sampTab sampTab
+#' @param dLevel dLevel
+#' @param topX topX
+#' @param dThresh dThresh
+#' @param alpha1 alpha1
+#' @param alpha2 alpha2
+#' @param mu mu
+#'
+#' @return list of cgenes and grps
+#' 
+#' @export
+findClassyGenes<-function(
+	expDat,
+	 sampTab,
+	 dLevel,
+	 topX=25,
+	 dThresh=0,
+	 alpha1=0.05,
+	 alpha2=.001,
+	 mu=2)
+{
+	gsTrain<-sc_statTab(expDat, dThresh=dThresh)
+	ggenes<-sc_filterGenes(gsTrain, alpha1=alpha1, alpha2=alpha2, mu=mu)
+	grps<-as.vector(sampTab[,dLevel])
+	names(grps)<-rownames(sampTab)
+	xdiff<-gnrAll(expDat[ggenes,], grps)
+	cgenes<-lapply(xdiff, getClassGenes, topX=topX)
+	cgenes<-unique(unlist(cgenes))
+	list(cgenes=cgenes, grps=grps)
+}
+
+#' makes complete gene-to-gene comparison
+#'
+#' @param expDat expDat
+#'
+#' @return list of list(genes=data frame, expDat=binary matrix)
+#'
+#' @export
+pair_transform<-function( # convert to a vector of length = length(vect)^2 - 1 /2
+expDat){
+	ngenes<-nrow(expDat)
+	genes<-rownames(expDat)
+	ans<-matrix(0, nrow=ngenes*(ngenes-1)/2, ncol=ncol(expDat))
+	pair_index<-1
+	genes1<-vector()
+	genes2<-vector()
+	for(i in 1:ngenes){
+		for(j in 1:ngenes){
+			if(j>i){
+				genes1<-append(genes1, genes[i])
+				genes2<-append(genes2, genes[j])
+				ans[pair_index,]<-as.numeric(expDat[i,]>expDat[j,]) 
+				pair_index<-pair_index +1
+			}
+		}
+	}
+	colnames(ans)<-colnames(expDat)
+	tList2 <- list(genes=data.frame(g1=genes1, g2=genes2), tDat=ans)
+
+	pairNames<-paste(tList2[[1]][,1], "_",tList2[[1]][,2], sep='')
+
+	pairDat<-tList2[[2]]
+	rownames(pairDat)<-pairNames
+	pairDat
+}
+
+
+#' weighted subtraction from mapped reades, applied to all
+#'
+#' Simulate expression profile of  _total_ mapped reads
+#' @param expRaw matrix of total mapped reads per gene/transcript
+#' @param total numeric post transformation sum of read counts
+#'
+#' @return vector of downsampled read mapped to genes/transcripts
+#'
+#' @export
+weighted_down<-function(
+	expRaw,
+ 	total,
+ 	dThresh=0
+ ){
+    expCountDnW<-apply(expRaw, 2, downSampleW, total=total, dThresh=dThresh)
+    #log(1+expCountDnW)
+    expCountDnW
+  }
+
+#' divide each column by sum of that column then scale to xFact and log it
+#'
+#' divide each column by sum of that column then scale to xFact and log it
+#' @param expDat expDat
+#' @param xFact xFact
+#'
+#' @return vector of downsampled read mapped to genes/transcripts
+#'
+#' @export
+trans_prop<-function(
+	expDat,
+ 	xFact=1e5
+){
+  ans<-matrix(0, nrow=nrow(expDat), ncol=ncol(expDat));
+  for(i in seq(ncol(expDat))){
+    ans[,i]<-expDat[,i]/sum(expDat[,i]);    
+  }
+  ans<-ans*xFact;
+  colnames(ans)<-colnames(expDat);
+  rownames(ans)<-rownames(expDat);
+  log(1+ans)
+}
+
+
+#
+#' make a classifier, with a randomized class too
+#'
+#' rmake a classifier, with a randomized class too
+#' @param expTrain training data
+#' @param genes vector of genes to use as predictors
+#' @param groups named vector of cells to groups or classes
+#' @param nRand =50 num of randomized profiles to make
+#' @param ntrees =2000 number of trees to build
+
+#' @return RF
+#' @export
+#'
+makeClassifier<-function(
+  expTrain,
+  genes,
+  groups,
+  nRand=50,
+  ntrees=2000){
+
+
+	randDat<-randomize(expTrain, num=nRand)
+	expTrain<-cbind(expTrain, randDat)
+
+	allgenes<-rownames(expTrain)
+
+	missingGenes<-setdiff(unique(genes), allgenes)
+	cat("Number of mussing genes ", length(missingGenes),"\n")
+	ggenes<-intersect(unique(genes), allgenes)
+	randomForest(t(expTrain[ggenes,]), as.factor(c(groups, rep("rand", ncol(randDat)))), ntree=ntrees)
+
+}
+
+#
+#' classify samples
+#'
+#' classify samples
+#' @param rfObj result of running sc_makeClassifier
+#' @param expQuery expQuery
+#' @param numRand numRand
+
+#' @return classRes matrix
+#' @export
+#'
+rf_classPredict<-function(
+  rfObj,
+  expQuery,
+  numRand=50){
+
+  	randDat<-randomize(expQuery, num=numRand)
+  	expQuery<-cbind(expQuery, randDat)
+
+    preds<-rownames(rfObj$importance)
+  	xpreds<-t(predict(rfObj, t(expQuery[preds,]), type='prob'))
+	colnames(xpreds)<-colnames(expQuery)
+	xpreds
+}
+
+
+#
+#' randomize data matrix 
+#'
+#' randomize data matrix 
+#' @param expDat expDat
+#' @param num number of profiles to return
+#'
+#' @return exp matrix random
+#' @export
+#'
+randomize<-function(
+ expDat,
+ num=50){
+
+
+	randDat<-t(apply(expDat, 1, sample))	
+ 	randDat<-apply(randDat, 2, sample)
+
+ 	randDat<-randDat[,sample(1:ncol(randDat), num)]
+	colnames(randDat)<-paste0(rep("rand_", num), 1:num)
+	rownames(randDat)<-rownames(expDat)
+	randDat
+}
+
+
+
+#' makes complete gene-to-gene comparison
+#'
+#' @param expDat expDat
+#' @param genePairs genePairs
+#'
+#' @return matrix indicating which gene of a pair is greater
+#'
+#' @export
+query_transform<-function( # convert to a vector of length = length(vect)^2 - 1 /2
+	expDat,
+ 	genePairs #vector of strings indicating pairs to compare
+ ){
+	genes<-strsplit(genePairs, "_")
+    ans<-matrix(0, nrow=length(genes), ncol=ncol(expDat))
+	pair_index<-1
+	genes1<-vector()
+	genes2<-vector()
+	for(i in seq(length(genes))){
+		ans[i,]<-as.numeric(expDat[genes[[i]][1],]>expDat[genes[[i]][2],])
+	}
+	colnames(ans)<-colnames(expDat)
+	rownames(ans)<-genePairs
+	ans
+}
+
+
+
+>>>>>>> parent of 590b901... Documentation
