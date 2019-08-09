ae8216a9de749718f019988272135e781621ff4a
Dan <dpeng5@jhu.edu>
2019-08-09 16:22:13 -0400

    cleaned subclass predict

--- R/subClass_predict.R
+++ R/subClass_predict.R
@@ -6,7 +6,7 @@
 #' @param cnProc_subclass the cnProc of the subclass classifier
 #' @param expDat the expression data of query data
 #' @param nrand the number of random profiles generate for evaluation process
-#' @param weight_broadClass the weight that will be put on the broadclass classification 
+#' @param weight_broadClass the weight that will be put on the broadclass classification
 #' @return a classification score matrix
 #'
 #' @export
@@ -14,9 +14,9 @@
 subClass_predict<-function(cnProc_broad, cnProc_sub, expDat, nrand = 2, weight_broadClass = 1) {
    ccnList = cnProc_sub # only concerns with subclass classifier
 
-   rf_tsp<-ccnList[['classifier']]
-   cgenes<-ccnList[['cgenes']]
-   xpairs<-ccnList[['xpairs']]
+   rf_tsp = ccnList[['classifier']]
+   cgenes = ccnList[['cgenes']]
+   xpairs = ccnList[['xpairs']]
 
    classMatrix = broadClass_predict(cnProc_broad, expDat = expDat, nrand = nrand)
    classMatrix = classMatrix[, -grep("rand", colnames(classMatrix))]
@@ -24,7 +24,7 @@ subClass_predict<-function(cnProc_broad, cnProc_sub, expDat, nrand = 2, weight_b
   if (weight_broadClass > 1) {
     print("Time to add weights")
     originalRowNames = rownames(classMatrix)
-    originalClassMatrix = classMatrix 
+    originalClassMatrix = classMatrix
     weightIter = c(1:(weight_broadClass - 1))
 
     for (weight in weightIter) {
