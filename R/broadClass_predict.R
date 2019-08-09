c134c654e077a79a3921d68cced074a35931e38d
Dan <dpeng5@jhu.edu>
2019-08-09 16:28:52 -0400

    cleaned broadClass_predict

--- R/broadClass_predict.R
+++ R/broadClass_predict.R
@@ -1,28 +1,28 @@
-#' @title 
-#' Predict query using broad class classifier 
+#' @title
+#' Predict query using broad class classifier
 #' @description
-#' The function predicts the query data using the broad class classifier 
-#' @param cnProc the cnProc of the broad classifier 
-#' @param expDat the expression data of query data 
-#' @param nrand the number of random profiles generate for evaluation process 
-#' @return a classification score matrix 
+#' The function predicts the query data using the broad class classifier
+#' @param cnProc the cnProc of the broad classifier
+#' @param expDat the expression data of query data
+#' @param nrand the number of random profiles generate for evaluation process
+#' @return a classification score matrix
 #'
-#' @export 
+#' @export
 broadClass_predict<-function(cnProc, expDat, nrand = 2) {
    ccnList = cnProc
-   expVal = expDat 
+   expVal = expDat
 
-   rf_tsp<-ccnList[['classifier']]
-   cgenes<-ccnList[['cgenes']]
-   xpairs<-ccnList[['xpairs']]
+   rf_tsp = ccnList[['classifier']]
+   cgenes = ccnList[['cgenes']]
+   xpairs = ccnList[['xpairs']]
 
    cat("Loaded in the cnProc\n")
 
-   expValTrans<-query_transform(expVal[cgenes,], xpairs)
-   classRes_val<-rf_classPredict(rf_tsp, expValTrans, numRand=nrand)
+   expValTrans = query_transform(expVal[cgenes,], xpairs)
+   classRes_val = rf_classPredict(rf_tsp, expValTrans, numRand=nrand)
 
    cat("All Done\n")
 
-   #return 
+   #return
    classRes_val
 }
