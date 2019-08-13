bb808f4edb0edd73711cf349e926a26c844a73b7
Dan Peng <dpeng5@jh.edu>
2019-08-13 15:55:11 -0400

    created new splitCommon function

---
+++ R/splitCommon_proportion.R
@@ -0,0 +1,31 @@
+#' @title
+#' Split training set and validation set
+#'
+#' @description
+#' Split the sample table into training and validation by proportion
+#'
+#' @param sampTab the sample table
+#' @param proportion the fraction of all available data in a category that you want to use for training
+#' @param dLevel the name of the sample table column with the classification categories
+#' @return a list containing training sample table and validation sample table
+#'
+#' @export
+splitCommon_proportion <- function(sampTab, proportion, dLevel) {
+  trainSampleTable = NULL
+  for(myCat in unique(sampTab[, dLevel])) {
+
+    tempSt = sampTab[sampTab[, dLevel] == myCat, ]
+    tempSt = tempSt[sample(rownames(tempSt), as.integer((proportion)*nrow(tempSt))), ]
+
+    if(is.null(trainSampleTable) == TRUE) {
+      trainSampleTable = tempSt
+    }
+    else {
+      trainSampleTable = rbind(trainSampleTable, tempSt)
+    }
+  }
+
+  valSampleTable = sampTab[!(rownames(sampTab) %in% rownames(trainSampleTable)), ]
+  # return
+  list("trainingSet" = trainSampleTable, "validationSet" = valSampleTable)
+}
