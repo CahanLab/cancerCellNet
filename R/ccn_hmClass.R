1d241743e4c69dc3c2b08d60551ddd4b83abb855
Dan <dpeng5@jhu.edu>
2020-01-09 11:32:22 -0500

    restructured simple heatmap plotting

--- R/ccn_hmClass.R
+++ R/ccn_hmClass.R
@@ -31,7 +31,22 @@ ccn_hmClass<-function(classMat, grps=NULL, isBig=FALSE, cRow=FALSE, cCol=FALSE, 
 
   # if no groups specified, use simple heatmap
   if(is.null(grps)){
-    cn_HmClass(classMat,isBig=isBig, cRow=cRow, cCol=cCol, fontsize_row=fontsize_row, fontsize_col=fontsize_col, main=main)
+
+    if(scale){
+      mymin<-min(classMat)
+      mymax<-max(classMat)
+    }else{
+      mymin<-0
+      mymax<-1
+    }
+
+    return(pheatmap::pheatmap(classMat,
+                              col=cools,
+                              breaks=seq(from=mymin, to=mymax, length.out=100),
+                              cluster_rows = cRow,
+                              cluster_cols = cCol,
+                              main = main, fontsize_row=fontsize_row, fontsize_col=fontsize_col,
+                              ...))
   }
 
   else{
@@ -67,62 +82,18 @@ ccn_hmClass<-function(classMat, grps=NULL, isBig=FALSE, cRow=FALSE, cCol=FALSE, 
 
     }
 
-    pheatmap::pheatmap(classMat,
+    return(pheatmap::pheatmap(classMat,
              col=cools,
              breaks=seq(from=mymin, to=mymax, length.out=100),
              cluster_rows = cRow,
              cluster_cols = cCol,
              show_colnames = FALSE,
              annotation_names_row = FALSE,
-             ##        annotation_col = annTab,
+
              annotation_col = xx,
              main = main,
              annotation_names_col = FALSE, annotation_colors = anno_colors, fontsize_row=fontsize_row, fontsize_col=fontsize_col,
-             ...)
+             ...))
   }
 
-}
-
-#' @title
-#' Simple heatmap of the classification result
-#'
-#' @description
-#' Heatmap of the classification result that does not have an explicit group.
-#'
-#' @param classMat classification matrix generated from \code{\link{rf_classPredict}}
-#' @param isBig TRUE if this is a big heatmap
-#' @param cRow TRUE if rows should be clustered
-#' @param cCol TRUE if columns should be clustered
-#' @param fontsize_row the font size of the row labels
-#' @param fontsize_col the font size of the column labels
-#' @param main the title of the heatmap
-#' @param scale FALSE if the highest value is 1 and the lowest is 0
-#'
-#' @return simpler classification heatmap without groups colored
-cn_HmClass<-function(classRes, isBig=FALSE, cRow=FALSE, cCol=FALSE, fontsize_row=4, fontsize_col=4, main=NA, scale=FALSE){
-  cools<-colorRampPalette(c("black", "limegreen", "yellow"))( 100 )
-  bcol<-'white';
-
-  if(isBig){
-    bcol<-NA;
-  }
-
-  if(scale){
-  mymin<-min(classMat)
-  mymax<-max(classMat)
-  }else{
-    mymin<-0
-    mymax<-1
-  }
-
-  pheatmap::pheatmap(classRes,
-           col=cools,
-           breaks=seq(from=mymin, to=mymax, length.out=100),
-           border_color=bcol,
-           cluster_rows = cRow,
-           cluster_cols = cCol,
-           main=main,
-           fontsize_row = fontsize_row,
-           fontsize_col = fontsize_col)
-  # classification heatmap
 }
